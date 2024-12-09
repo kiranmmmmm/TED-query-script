f73637908fe19fae8f331cc03e4e2ff420cb4573
Kiran M <kiran_muthukrishnan@outlook.com>
2024-12-09 12:10:26 +1100

    testing/debugging api_script

--- api_script.py
+++ api_script.py
@@ -1,5 +1,19 @@
+# importing packages
 import requests
-import pandas as pd
-response = requests.get("https://ted.cathdb.info//api/v1/uniprot/summary/Q13201")
-myfamily = response.json()
-print(myfamily)
+# import pandas as pd
+import subprocess
+
+# running retrieve_id file using run()
+subprocess.run(["python","retrieve_id.py"])
+
+# creating myfamily to append all relevant protein data via API query using acc_nos generated from retrieve_id.py
+# myfamily = {}
+
+# iterating through list of accession numbers and appending to myfamily
+""" for i in acc_nos:
+    response = requests.get("https://ted.cathdb.info//api/v1/uniprot/summary/{i}")
+    prot = response.json()
+    myfamily[i] = prot
+
+print(myfamily) """
+
--- retrieve_id.py
+++ retrieve_id.py
@@ -1,10 +1,10 @@
 # Import json file of desired protein information loaded from Uniprot, to extract Uniprot ids (accession number) into a list
 import json
 
-with open('uniprot_file.json') as j:
+with open('uniprotkb_test.json') as j:
     json_data = json.load(j)
 
-# retrieve all values attributed to key = "secondaryAccessions"
-acc_nos = [result["secondaryAccessions"] for result in json_data["results"]]
+# retrieve all values attributed to key = "primaryAccession"
+acc_nos = [result["primaryAccession"] for result in json_data["results"]]
 
 print(acc_nos)
