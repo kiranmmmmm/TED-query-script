29d072258602750295e2d1bca388f002aafbf2ff
Kiran M <kiran_muthukrishnan@outlook.com>
2024-12-09 16:20:40 +1100

    Update test_script_TED_files.py
    work in progress

--- test_script_TED_files.py
+++ test_script_TED_files.py
@@ -1,6 +1,30 @@
-import requests
+# import requests
 import json
+import os
+import sys
+import itertools
 
-from test_script_api import myfamily
+# retrieve myfamily
+# from test_script_api import myfamily
 
-ted_nos = [result["primaryAccession"] for result in json_data["results"]]
+# test version so I don't borther the API too much :)
+with open('test_family.json') as j:
+    my_family = json.load(j)
+
+ted_dict = {}
+recursive_dict = {}
+
+# directory = "D:/Scripts/data/test_ted"
+directory = input("please state address of EXISTING root folder for writing in the pdb file database: ")
+directory = os.path.abspath(directory)
+seperator = "________________________________________________________________________________________________________________________"
+
+if not os.path.exists(directory):
+    print("the address entered does not exist or is incorrectly formatted.")
+    print("creating new folder in current working directory")
+    directory = os.getcwd() + "/ted_output"
+    os.makedirs(directory)
+
+""" ted_nos = [result["ted_id"] for result in myfamily["results"]]
+
+print(ted_nos) """
