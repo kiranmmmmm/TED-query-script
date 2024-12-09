cf26e82ff85b5e6e366111c9b3fd1e33f5b72bd2
Kiran M <kiran_muthukrishnan@outlook.com>
2024-12-09 11:29:29 +1100

    Updating with new script for getting accession numbers

--- api_script.py
+++ api_script.py
@@ -2,5 +2,4 @@ import requests
 import pandas as pd
 response = requests.get("https://ted.cathdb.info//api/v1/uniprot/summary/Q13201")
 myfamily = response.json()
-objects = pd.json_normalize(myfamily, record_path='data', meta=['id','sort','name'], errors='ignore', meta_prefix='data_')
-objects.explode('values')
+print(myfamily)
