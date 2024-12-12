e9ccae1c3195024966991235d1460e62ccf6f023
Kiran M <kiran_muthukrishnan@outlook.com>
2024-12-12 12:33:37 +1100

    Update script_api.py

--- script_api.py
+++ script_api.py
@@ -9,10 +9,10 @@ from script_retrieve_id import acc_nos
  
 print(acc_nos)
 
-# creating myfamily to append all relevant protein data via API query using acc_nos generated from retrieve_id.py
+# creating dictionary to append all relevant protein data via API query using acc_nos generated from retrieve_id.py
 myfamily = {}
 
-# iterating through list of accession numbers and appending to myfamily
+# iterating through list of accession numbers and appending to dictionary
 for i in acc_nos:
     url = f'https://ted.cathdb.info//api/v1/uniprot/summary/{i}'
     response = requests.get(url)
