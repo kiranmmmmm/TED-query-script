3f3580c5c674b37abf132a1d1d9859fe9aaec827
Kiran M <kiran_muthukrishnan@outlook.com>
2024-12-13 11:15:08 +1100

    finalising for non-test application

--- script_retrieve_id.py
+++ script_retrieve_id.py
@@ -1,7 +1,10 @@
 # Import json file of desired protein information loaded from Uniprot, to extract Uniprot ids (accession number) into a list
 import json
 
-with open('uniprotkb_test.json') as j:
+#state custom file path for json with all protein data for desired database
+file = input("Please state the address of Uniprot-downloaded json file for accession ID extraction: ")
+
+with open(file) as j:
     json_data = json.load(j)
 
 # retrieve all values attributed to key = "primaryAccession"
