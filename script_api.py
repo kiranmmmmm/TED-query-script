0652a0c5fa2b8859fa021424da404e09aaa69549
kiranmmmmm <kmut6307@uni.sydney.edu.au>
2024-12-30 08:45:33 +0530

    Added file
    attempting to create script to write a metadata json file to complement the dataset generated.

--- script_api.py
+++ script_api.py
@@ -19,23 +19,6 @@ myfamily = {}
 addfamily1 = []
 addfamily2 = []
 
-# janky as fk method to pause all threads for 1 minute after 100 iterations
-""" a = 0
-b = 1
-c = 2
-d = 3
-e = 4
-f = 5
-g = 6
-h = 7
-i = 8
-j = 9
-k = 10
-l = 11
-m = 12
-n = 13
-o = 14 """
-
 # creating session with way less max_retries so that 443 errors can be detected faster and passed quicker
 s = requests.Session()
 retries = Retry(total=2,
